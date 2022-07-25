813056a15a07e86c72f8410b686178dc56beaea9
George Murdocca <george@linuxdojo.com>
2022-07-25 10:44:13 +1000

    improve logging

--- evon/client.py
+++ evon/client.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
+#################################
+# EVON CLI
+#################################
+
+
 import logging
 import logging.handlers
+import os
+import pkg_resources
 import sys
 
 import requests
@@ -11,17 +18,25 @@ from evon import mapper
 
 # setup logging
 logger = logging.getLogger()
+logger.handlers = []
 logger.setLevel(logging.INFO)
 syslog_handler = logging.handlers.SysLogHandler(address = '/dev/log')
 stdout_handler = logging.StreamHandler(sys.stdout)
-fmt = logging.Formatter(fmt='evon[%(process)d]: %(levelname)s: %(message)s')
-syslog_handler.setFormatter(fmt)
-stdout_handler.setFormatter(fmt)
+syslog_fmt = logging.Formatter(fmt='evon[%(process)d]: %(levelname)s: %(message)s')
+stdout_fmt = logging.Formatter(fmt='%(asctime)s - %(levelname)s: %(message)s')
+syslog_handler.setFormatter(syslog_fmt)
+stdout_handler.setFormatter(stdout_fmt)
 logger.addHandler(syslog_handler)
+logger.addHandler(stdout_handler)
+
+EVON_DEBUG = os.environ.get('EVON_DEBUG', '').upper() == "TRUE"
+if EVON_DEBUG:
+    logger.setLevel(logging.DEBUG)
+EVON_VERSION = pkg_resources.require('evon')[0].version
 
 
 def main():
-    logger.info(f"evon client starting - {sys.version}")
+    logger.info(f"Evon client v{EVON_VERSION} starting - {sys.version}")
 
 
 if __name__ == "__main__":
