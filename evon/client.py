91201adf5f6d4db9bea8d47ee4838aa9df9c021c
George Murdocca <george@linuxdojo.com>
2022-07-29 21:05:40 +1000

    add logging and api libs

--- evon/client.py
+++ evon/client.py
@@ -13,22 +13,9 @@ import sys
 
 import requests
 
-from evon import mapper
+from evon import log, api
 
-
-# setup logging
-logger = logging.getLogger()
-logger.handlers = []
-logger.setLevel(logging.INFO)
-syslog_handler = logging.handlers.SysLogHandler(address = '/dev/log')
-stdout_handler = logging.StreamHandler(sys.stdout)
-syslog_fmt = logging.Formatter(fmt='evon[%(process)d]: %(levelname)s: %(message)s')
-stdout_fmt = logging.Formatter(fmt='%(asctime)s - %(levelname)s: %(message)s')
-syslog_handler.setFormatter(syslog_fmt)
-stdout_handler.setFormatter(stdout_fmt)
-logger.addHandler(syslog_handler)
-logger.addHandler(stdout_handler)
-
+logger = log.get_evon_logger()
 EVON_DEBUG = os.environ.get('EVON_DEBUG', '').upper() == "TRUE"
 if EVON_DEBUG:
     logger.setLevel(logging.DEBUG)
