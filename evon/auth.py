cbaeb5f6dace43fd11e913049607cb4dc0f7358c
George Murdocca <george@linuxdojo.com>
2022-08-25 21:52:36 +1000

    - move dockerfile to separate git repo - improve os support in bootstrap - begin auth module

---
+++ evon/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+
+
+username = os.environ["username"]
+password = os.environ["password"]
+
+# TODO
+# assert username is a UUID
+# assert password is md5sum(UUID + ec2_id)
+# assert username is not currently connected
+# if mode == discovery
+#   allow any username, create CCD entry if not present
+#   if ip in pool, bounce connection
+# elif mode == operational:
+#    assert username present in CD
+
+
