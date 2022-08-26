121bdde0a6fb9dd958409e358131ac6c26bc090c
George Murdocca <george@linuxdojo.com>
2022-08-26 12:41:06 +1000

    add api functions

--- api/evon/hub/views.py
+++ api/evon/hub/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import viewsets
+from django.shortcuts import render
 from hub import models
 from hub.api import serializers
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 
 ##### App Views ####
@@ -14,25 +15,47 @@ def index(request):
 ##### API Views ####
 
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    Evon users
+    """
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Evon user groups
+    """
     queryset = models.Group.objects.all()
     serializer_class = serializers.GroupSerializer
 
 
 class ServerViewSet(viewsets.ModelViewSet):
+    """
+    Evon connected servers
+    """
     queryset = models.Server.objects.all()
     serializer_class = serializers.ServerSerializer
 
 
 class ServergroupViewSet(viewsets.ModelViewSet):
+    """
+    Evon server groups
+    """
     queryset = models.Servergroup.objects.all()
     serializer_class = serializers.ServergroupSerializer
 
 
 class PolicyViewSet(viewsets.ModelViewSet):
+    """
+    Evon permissions policies
+    """
     queryset = models.Policy.objects.all()
     serializer_class = serializers.PolicySerializer
+
+class HelloViewSet(viewsets.ViewSet):
+    """
+    Custom hello endpoint
+    """
+    def list(self, response):
+        return Response("hello world")
