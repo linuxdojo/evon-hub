356dac608deddaf0264ff228e5a77e03741ea1fd
George Murdocca <george@linuxdojo.com>
2022-09-06 14:24:58 +1000

    switch django api to mariadb, fix eapi cli

--- eapi/asgi.py
+++ eapi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for evon project.
+ASGI config for eapi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evon.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eapi.settings")
 
 application = get_asgi_application()
