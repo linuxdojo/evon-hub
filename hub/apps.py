54493eee61772f8e51c2d1f1e07511f88e05aa68
George Murdocca <george@linuxdojo.com>
2022-09-07 22:50:47 +1000

    add authtoken to api, switch users/groups to django contrib.auth.models, add template overrides for drf, update initial migration

--- hub/apps.py
+++ hub/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HubConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "hub"
+
+    def ready(self):
+        from . import signals
--- hub/models.py
+++ hub/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 
-class User(models.Model):
-    user_name = models.CharField(max_length=200)
-    create_date = models.DateTimeField('date created')
-
-    def __str__(self):
-        return self.user_name
-
-
-class Group(models.Model):
-    group_name = models.CharField(max_length=200)
-    create_date = models.DateTimeField('date created')
-
-    def __str__(self):
-        return self.group_name
-
-
 class Server(models.Model):
     fqdn = models.CharField(max_length=1004)
     ipv4_address = models.CharField(max_length=15)
