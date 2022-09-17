93602fa09fd2ed45c686a945325c1ee49f6ecbcf
George Murdocca <george@linuxdojo.com>
2022-09-17 13:34:55 +1000

    add server model validators

--- hub/models.py
+++ hub/models.py
@@ -1,4 +1,7 @@
+import ipaddress
+import os
 import re
+import yaml
 
 from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
@@ -6,25 +9,61 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch.dispatcher import receiver
+from django.core.exceptions import ValidationError
 
+from eapi.settings import BASE_DIR
+
+
+##### Setup globals
 
 FQDN_PATTERN = re.compile(r'(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)')
 UUID_PATTERN = re.compile(r'^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}$')
+with open(os.path.join(BASE_DIR, "evon-vars.yaml")) as f:
+    evon_vars = yaml.safe_load(f)
 
+
+##### Model Validators
+
+def EvonIPV4Validator(value):
+    subnet_key = evon_vars["subnet_key"]
+    evon_subnet = f"100.{subnet_key}.224.0/19"
+    if not ipaddress.ip_address(value) in ipaddress.ip_network(evon_subnet):
+        raise ValidationError(
+            f"IPv4 address must be in subnet {evon_subnet}"
+        )
+
+
+def EvonFQDNValidator(value):
+    if not FQDN_PATTERN.match(value):
+        raise ValidationError(
+            "Please provide a valid FQDN"
+        )
+    evon_account_domain = evon_vars["account_domain"]
+    if not value.lower().endswith(evon_account_domain):
+        raise ValidationError(
+            f"Provided FQDN must end with '.{evon_account_domain}'"
+        )
+
+
+##### Model Classes
 
 class Server(models.Model):
     # Max fqdn length is 1004 according to RFC, but max mariadb unique varchar is 255
-    ipv4_address = models.GenericIPAddressField(protocol="IPv4")
+    ipv4_address = models.GenericIPAddressField(
+        protocol="IPv4",
+        validators=[EvonIPV4Validator]
+    )
     fqdn = models.CharField(
         max_length=255,
         unique=True,
-        validators=[RegexValidator(regex=FQDN_PATTERN)],
+        validators=[EvonFQDNValidator],
     )
     uuid = models.CharField(
         max_length=36,
         unique=True,
         validators=[RegexValidator(regex=UUID_PATTERN)],
     )
+    connected = models.BooleanField(default=False, editable=False)
 
     def __str__(self):
         return self.fqdn
@@ -51,6 +90,8 @@ class Policy(models.Model):
     class Meta:
         verbose_name_plural = "Policies"
 
+
+##### Signal handlers
 
 @receiver(pre_delete, sender=User)
 def delete_user(sender, instance, **kwargs):
