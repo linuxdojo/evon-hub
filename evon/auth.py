f996ee954c9d17d0b8a191d24dc4811e7c41a7a8
George Murdocca <george@linuxdojo.com>
2022-09-18 09:39:09 +1000

    partial implementation of openvpn auth script, rename auditlog app in django admin

--- evon/auth.py
+++ evon/auth.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
 import os
+import re
+import sys
 
+import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'eapi.settings'
+django.setup()
+from django.contrib.auth.models import User
+from hub.models import UUID_PATTERN
+
+from hub.log import get_evon_logger
 
 username = os.environ["username"]
 password = os.environ["password"]
 
-# TODO
+logger = get_evon_logger()
+
+if not UUID_PATTERN.match(username):
+    logger.info(f"Denying login for user '{username}': Username is not a valid UUIDv4 string")
+    sys.exit(1)
 # assert username is a UUID
 # assert password is md5sum(UUID + ec2_id)
 # assert username is not currently connected
