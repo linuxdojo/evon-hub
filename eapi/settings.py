9d3633979f7c51801b2dfd2937a053eec32baa59
George Murdocca <george@linuxdojo.com>
2022-09-18 14:16:55 +1000

    implement openvpn auth script

--- eapi/settings.py
+++ eapi/settings.py
@@ -86,6 +86,7 @@ JAZZMIN_SETTINGS = {
         "auth.user": "fas fa-user",
         "hub.policy": "fas fa-shield-alt",
         "hub.server": "fas fa-server",
+        "hub.config": "fas fa-cog",
         "hub.servergroup": "fas fa-network-wired",
     },
 }
