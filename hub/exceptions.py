6dd0314fbf6bb462f5aef97dc0d4b51419c877f5
George Murdocca <george@linuxdojo.com>
2022-09-23 21:07:02 +1000

    update server model on auth

---
+++ hub/exceptions.py
@@ -0,0 +1,3 @@
+
+class OutOfAddresses(Exception):
+    pass
--- hub/signals.py
+++ hub/signals.py
@@ -1,10 +1,22 @@
-from django.db.models.signals import post_save
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-from django.conf import settings
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+@receiver(pre_delete, sender=User)
+def delete_user(sender, instance, **kwargs):
+    """
+    Ensure admin and deployer are immutable
+    """
+    #TODO this produces an empty 403 Forbidden page. Consider how to improve it.
+    if instance.username in ["admin", "deployer"]:
+        raise PermissionDenied
