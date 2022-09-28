b11d328c0e757a936e271c3cd99d46c651c460d2
George Murdocca <george@linuxdojo.com>
2022-09-29 09:19:08 +1000

    add auditlog flush function for 90 days retention

---
+++ evon/hourly_job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+AUDITLOG_RETENTION_DAYS=90
+
+# Sync all Evon-connected servers to reflect current connected state
+/usr/local/bin/evon --sync-servers
+
+#  Delete auditlogs older than AUDITLOG_RETENTION_DAYS days
+/usr/local/bin/eapi auditlogflush -y -b $(date -d "-${AUDITLOG_RETENTION_DAYS} days" "+%Y-%m-%d")
