0a4d924c08db69422961fed63693d7078207ebfd
George Murdocca <george@linuxdojo.com>
2022-09-28 23:18:34 +1000

    improve default user and group management in admin

--- hub/signals.py
+++ hub/signals.py
@@ -8,7 +8,7 @@ from rest_framework.authtoken.models import Token
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
+    if created and instance.username in ["admin", "deployer"]:
         Token.objects.create(user=instance)
 
 
