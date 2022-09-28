ddf414a9706b58d0e11ed54740138b090abe6fb0
George Murdocca <george@linuxdojo.com>
2022-09-28 22:24:16 +1000

    implement more detailed models for server, servergroup, policy. Improve django admin.

--- TODO.md
+++ TODO.md
@@ -6,19 +6,11 @@
 
 ## OpenVPN
 
-* ✅ create auth-user-pass-verify script for `evon-hub/templates/openvpn/server/server_tcp.conf`
- * ❌ check for duplicate (currently connected) uuid and reject if so (blocked: ovpn mgmt interface hangs during auth exec)
- * ✅ consider discovery|operational modes via custom login scripts for server acquisition vs. changeless op mode
-* ✅ on Evon Hub service start: sync all connected state in all django Server objects (consider concurrency)
 * add an auth script for UDP that checks against a dango user
 
 ## Policy
 
 * implement
-
-## Cloud
-
-* ✅ actuate Route53 changes on Server.save() for connect=True/False
 
 ## Miscelaneous
 
@@ -28,8 +20,6 @@
 * address TODO, FIXME, XXX comments
 * in django admin, show related properties of user models, ie. show tokens in the auth.Users app
 * default admin password is ec2 id. Force change first login.
-* ✅ rename systemd service to evonhub
 * in bootstrap, send enc payload to hub for decrypt, with optional local decrypt (use same EVON_DEPLOY_KEY for both, try local decrypt, then remote).
-* ✅ add max server count check to auth.py
 * implement last_seen on server objects
 * consider redirecting all hub urls to django app in nginx. SSL certs and ALLOWED_HOSTS needs to be managed accordingly. Alternate/simpler: allow clients to choose their own domain prefix rather than the 5 character auto-generated one.
--- hub/admin.py
+++ hub/admin.py
@@ -3,27 +3,30 @@ from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.db import models
 from django.forms.widgets import Select
 from django.utils import timezone
-import humanfriendly
 from solo.admin import SingletonModelAdmin
+import humanfriendly
 
 import hub.models
 
 
-@admin.register(hub.models.ServerGroup)
 @admin.register(hub.models.Policy)
+class PolicyAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(hub.models.Config)
-class ModelAdmin(admin.ModelAdmin):
+class ConfigAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(hub.models.Server)
 class ServerAdmin(admin.ModelAdmin):
     readonly_fields = ('uuid', 'fqdn','ipv4_address', 'connected', 'disconnected_since', 'last_seen')
-    list_display = ['fqdn', 'uuid', 'ipv4_address', 'connected', 'disconnected_since', 'last_seen']
+    list_display = ['fqdn', 'uuid', 'ipv4_address', 'connected', 'disconnected_since', 'last_seen', 'groups']
 
     def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
         extra_context = extra_context or {}
-        extra_context['show_save'] = False
+        extra_context['show_save'] = True
         extra_context['show_save_and_continue'] = False
         return super().changeform_view(request, object_id, form_url, extra_context)
 
@@ -38,3 +41,17 @@ class ServerAdmin(admin.ModelAdmin):
             delta_seconds = round(delta.total_seconds())
             hf_delta = humanfriendly.format_timespan(delta_seconds, detailed=False, max_units=2)
             return f"{hf_delta} ago"
+
+    def groups(self, obj):
+        return ", ".join(sg.name for sg in obj.server_groups.all())
+
+
+class ServerInline(admin.TabularInline):
+    model = hub.models.Server.server_groups.through
+
+
+@admin.register(hub.models.ServerGroup)
+class ServerGroupAdmin(admin.ModelAdmin):
+    inlines = [
+        ServerInline,
+    ]
