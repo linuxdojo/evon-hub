f595cbe1970b6d6ed716a0221cc4e3c8baf0a8f0
George Murdocca <george@linuxdojo.com>
2022-10-04 21:59:24 +1100

    make config object immutable in admin (edit/save permitted only)

--- hub/admin.py
+++ hub/admin.py
@@ -184,7 +184,18 @@ class PolicyAdmin(admin.ModelAdmin):
 
 @admin.register(hub.models.Config)
 class ConfigAdmin(admin.ModelAdmin):
-    pass
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['show_save_and_continue'] = False
+        extra_context['show_save_and_add_another'] = False
+        return super().changeform_view(request, object_id, form_url, extra_context)
 
 
 @admin.register(hub.models.Server)
