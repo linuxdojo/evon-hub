8d3e791e899e08fd6b7a01b6c5d4df0734272d3c
George Murdocca <george@linuxdojo.com>
2022-10-05 15:08:36 +1100

    add timeouts to requests

--- evon/evon_api.py
+++ evon/evon_api.py
@@ -18,12 +18,19 @@ EVON_DEBUG = os.environ.get('EVON_DEBUG', '').upper() == "TRUE"
 if EVON_DEBUG:
     logger.setLevel(logging.DEBUG)
 API_URL = os.environ.get("EVON_API_URL")
+REQUESTS_TIMEOUT = 10
 
 
 def generate_headers(api_key):
-    response = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
+    response = requests.get(
+        "http://169.254.169.254/latest/dynamic/instance-identity/document",
+        timeout=REQUESTS_TIMEOUT
+    )
     iid = base64.b64encode(response.text.encode("utf-8"))
-    response = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/signature")
+    response = requests.get(
+        "http://169.254.169.254/latest/dynamic/instance-identity/signature",
+        timeout=REQUESTS_TIMEOUT
+    )
     iid_signature = response.text.replace("\n", "")
     headers = {
         "Content-Type": "application/json",
@@ -36,7 +43,10 @@ def generate_headers(api_key):
 
 
 def get_pub_ipv4():
-    response = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4")
+    response = requests.get(
+        "http://169.254.169.254/latest/meta-data/public-ipv4",
+        timeout=REQUESTS_TIMEOUT
+    )
     return response.text
 
 
@@ -47,7 +57,7 @@ def do_request(url, requests_method, headers, json_payload=None):
     if json_payload:
         request_kwargs["data"] = json_payload.encode("utf-8")
     try:
-        response = requests_method(url, **request_kwargs)
+        response = requests_method(url, **request_kwargs, timeout=REQUESTS_TIMEOUT)
         response.raise_for_status()
     except requests.exceptions.HTTPError as e:
         logger.error(f"{requests_method.__name__.upper()} request failed: '{e}' ")
@@ -56,12 +66,21 @@ def do_request(url, requests_method, headers, json_payload=None):
 
 def get_records(api_url, api_key):
     url = f"{api_url}/zone/records"
-    response = do_request(url, requests.get, headers=generate_headers(api_key))
+    response = do_request(
+        url,
+        requests.get,
+        headers=generate_headers(api_key)
+    )
     records = json.dumps(json.loads(response.text), indent=2)
     return records
 
 
 def set_records(api_url, api_key, json_payload):
     url = f"{api_url}/zone/records"
-    response = do_request(url, requests.put, headers=generate_headers(api_key), json_payload=json_payload)
+    response = do_request(
+        url,
+        requests.put,
+        headers=generate_headers(api_key),
+        json_payload=json_payload
+    )
     return response.text
