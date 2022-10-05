f341034aefbab495c019823d65d5cb38ed15f47a
George Murdocca <george@linuxdojo.com>
2022-10-05 15:09:15 +1100

    setup automatic All Users group and All Servers group

--- hub/apps.py
+++ hub/apps.py
@@ -9,6 +9,25 @@ class HubConfig(AppConfig):
 
     def ready(self):
         from . import signals
+        from . import models
+
+        # create All Users group
+        group, created = models.Group.objects.update_or_create(
+            name="All Users",
+        )
+        all_users = models.User.objects.all()
+        for user in all_users:
+            if user not in group.user_set.all():
+                user.groups.add(group)
+
+        # create All Servers group
+        server_group, created = models.ServerGroup.objects.update_or_create(
+            name="All Servers",
+        )
+        all_servers = models.Server.objects.all()
+        for server in all_servers:
+            if server not in server_group.server_set.all():
+                server.server_groups.add(server_group)
 
 
 # Example of renaming of 3rd party app in Django Admin
