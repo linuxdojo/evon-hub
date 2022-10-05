5bffd632a844e97f4422999304e9661fc3012f63
George Murdocca <george@linuxdojo.com>
2022-10-05 15:09:15 +1100

    setup automatic All Users group and All Servers group

--- hub/signals.py
+++ hub/signals.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.signals import user_logged_in
 from django.core.exceptions import PermissionDenied
 from django.db.models.signals import pre_delete, post_save
@@ -9,23 +9,41 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 from eapi.settings import EVON_VARS
+from hub.models import Server, ServerGroup
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
-    # every new user gets an api token
+    # every new user gets an api token and gets added to All Users group
     if created:
+        # create token
         Token.objects.create(user=instance)
+        # add to group
+        all_users_group = Group.objects.get(name="All Users")
+        instance.groups.add(all_users_group)
 
+@receiver(post_save, sender=Server)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        all_servers_group = ServerGroup.objects.get(name="All Servers")
+        instance.server_groups.add(all_servers_group)
+
+@receiver(pre_delete, sender=Group)
+def delete_group(sender, instance, **kwargs):
+    if instance.name == "All Users":
+        raise PermissionDenied
+
+@receiver(pre_delete, sender=ServerGroup)
+def delete_server_group(sender, instance, **kwargs):
+    if instance.name == "All Servers":
+        raise PermissionDenied
 
 @receiver(pre_delete, sender=User)
 def delete_user(sender, instance, **kwargs):
     """
     Ensure admin and deployer are immutable
     """
-    #XXX this produces an empty 403 Forbidden page in Admin. Consider how to improve it.
     if instance.username in ["admin", "deployer"]:
         raise PermissionDenied
-
 
 @receiver(user_logged_in)
 def post_login(sender, user, request, **kwargs):
