c26036a7127f46daa0c4dbd00bc13430dc1a6bfd
George Murdocca <george@linuxdojo.com>
2022-10-11 19:16:56 +1100

    implement static ipv4 addresses for users

---
+++ evon/openvpn_scripts/user_connect.py
@@ -0,0 +1,48 @@
+#!/opt/evon-hub/.env/bin/python
+
+#######################################
+# Evon OpenVPN User Connect Script
+#######################################
+
+
+import ipaddress
+import os
+import sys
+
+import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'eapi.settings'
+django.setup()
+from hub.models import User  # noqa
+from evon.log import get_evon_logger  # noqa
+
+
+logger = get_evon_logger()
+if not len(sys.argv) >= 2:
+    logger.error("Did not receive any args")
+    sys.exit(1)
+ccd_file = sys.argv[-1]
+cn = os.environ["common_name"]
+
+logger.info(f"Setting up CCD file for user with common name: {cn}")
+
+if not os.access(ccd_file, os.W_OK):
+    logger.error(f"Specified CCD file not writable: {ccd_file}")
+    sys.exit(1)
+
+if os.path.getsize(ccd_file):
+    logger.error(f"Specified CCD file not empty: {ccd_file}")
+    sys.exit(1)
+
+user = User.objects.filter(username=cn).first()
+if not user:
+    logger.error(f"User with username '{cn}' does not exist")
+    sys.exit(1)
+
+# generate dynamic CCD content for client
+local = user.userprofile.ipv4_address
+remote = ipaddress.ip_address(local) - 1
+ccd_config = f'ifconfig-push {local} {remote}'
+with open(ccd_file, "w") as f:
+    f.write(ccd_config)
+
+logger.info(f"Wrote CCD file '{ccd_file}' with content: {ccd_config}")
