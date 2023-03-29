1dfcc716695f6a8411cbe523a2693505aec4279b
George Murdocca <george@linuxdojo.com>
2023-03-29 23:31:05 +1100

    fixes for ansible and evonshim

--- evon/selfhosted_shim/evonshim.sh
+++ evon/selfhosted_shim/evonshim.sh
@@ -5,7 +5,7 @@ if [ "$1" == "start" ]; then
     exec /opt/evon-hub/.env/bin/uvicorn \
         evon.selfhosted_shim.server:app \
         --host 169.254.169.254 \
-        --port 80
+        --port 8000
 elif [ "$1" == "stop" ]; then
     /usr/sbin/ip addr del 169.254.169.254/32 dev lo >/dev/null 2>&1 || :
     /bin/kill -s TERM $2
