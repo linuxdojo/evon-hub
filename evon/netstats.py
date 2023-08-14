e1aefb0987c7d66be79b9f0ba279621e50978098
George Murdocca <george@linuxdojo.com>
2023-08-15 00:42:15 +1000

    add vnstat cli tool and parser to extract bandwidth usage data

--- evon/netstats.py
+++ evon/netstats.py
@@ -1,28 +1,84 @@
 #!/usr/bin/env python
 
-import psutil
+from collections import defaultdict
+import datetime
+import os
+import json
+import subprocess
 
-# Must run as root
+import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'eapi.settings'  # noqa
+django.setup()  # noqa
+
+from evon.log import get_evon_logger  # noqa
+from hub.models import User  # noqa
+
+
+logger = get_evon_logger()
 
 def get_interfaces():
     """
-    returns a list of non-loopback interfaces that are up
+    returns a list of non-loopback interfaces
     """
-    addresses = psutil.net_if_addrs()
-    stats = psutil.net_if_stats()
-    interfaces = []
-    for interface, addr_list in addresses.items():
-        if any(getattr(addr, 'address').startswith("169.254") for addr in addr_list):
+    # just shell out, as python's psutil requires root/capabilities to read flags like LOOPBACK
+    cmd = "ip link show | grep -E '^[0-9]' | grep -v LOOPBACK | cut -d: -f2 | xargs"
+    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+    stdout_err = p.stdout.read().decode("utf-8").strip()
+    return stdout_err.split()
+
+
+def get_user_count():
+    """
+    Returns current user count (less 2, for admin and deployer) on the hub
+    """
+    return User.objects.count() - 2
+
+
+def get_data_used_in_month():
+    """
+    returns data used in month in MB, rounded
+    """
+    bytes_used = 0
+    for interface in get_interfaces():
+        cmd = f"vnstat {interface} --oneline b"
+        p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+        stdout_err = p.stdout.read().decode("utf-8").strip()
+        if "not enough data" in stdout_err.lower():
             continue
-        elif interface in stats \
-            and getattr(stats[interface], "isup") \
-            and "loopback" not in getattr(stats[interface], 'flags'):
-            interfaces.append(interface)
-    return interfaces
+        bytes_used += int(stdout_err.split(";")[9])
+    return int(round(bytes_used / 1024))
 
 
-# TODO use vnstat to collect 5-minutely, daily and monthly TX data counts
+def get_data_used_per_day():
+    """
+    Returns daily data used in current month in MB in format:
+    {"<day_of_month> <3_letter_month>": "<mb_used>", ...}
+    """
+    first_day_of_month_ts = f"{datetime.datetime.now().strftime('%Y-%m')}-01 00:00"
+    cmd = f'vnstat --begin "{first_day_of_month_ts}" --json d'
+    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+    stdout_err = p.stdout.read().decode("utf-8")
+    try:
+        all_stats = json.loads(stdout_err)["interfaces"]
+    except Exception:
+        return {}
+    result = defaultdict(int)
+    all_interfaces = get_interfaces()
+    for interface_stats in all_stats:
+        if interface_stats.get("name") in all_interfaces:
+            for day_data in interface_stats["traffic"]["day"]:
+                month_num = str(day_data["date"]["month"])
+                month_short_name = datetime.datetime.strptime(month_num, "%m").strftime("%b")
+                day = day_data["date"]["day"]
+                key = f"{day} {month_short_name}"
+                val = int(round(day_data["tx"] / 1024))
+                result[key] += val
+    return dict(result)  # oerdered since Python 3.7
+
+# TODO store values in DDB
 
 if __name__ == "__main__":
-    interfaces = get_interfaces()
-    print(interfaces)
+    print("Users:", get_user_count())
+    print("Data used in month to date (MB):", get_data_used_in_month())
+    print("Daily data used in month to date (MB):", get_data_used_per_day())
+
--- support/package_template.sh
+++ support/package_template.sh
@@ -386,7 +386,8 @@ elif [ $os_version -eq 8 ]; then
         openssl-devel
         python3-certbot-nginx
         sslh
-        tar"
+        tar
+        vnstat"
     dnf -y install epel-release
     dnf -y install $package_list
 else
@@ -404,7 +405,8 @@ else
         mariadb-server
         openssl-devel
         python3-certbot-nginx
-        tar"
+        tar
+        vnstat"
     dnf config-manager --set-enabled crb
     dnf -y install epel-release
     dnf -y install $package_list
@@ -472,12 +474,17 @@ chmod 4755 /usr/local/bin/eapi
 source /opt/evon-hub/evon/.evon_env
 
 if [ "$SELFHOSTED" == "true" ]; then
+
     # we're selfhosted, start and persist the selfhosted_shim service
     setenforce 0 || :
     sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
     cd /opt/evon-hub/evon/selfhosted_shim
     make deploy
     cd -
+
+    # also start and persist the vnstat service
+    systemctl start vnstat
+    systemctl enable vnstat
 fi
 
 echo '### Obtaining and persisting account info...'
