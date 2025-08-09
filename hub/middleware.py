c2acbf63e4e067c1f7dd08b387a99a3af10de2a4
George Murdocca <george@linuxdojo.com>
2025-08-09 22:48:47 +1000

    Update condition to bypass IAM Role warning if deployment is not via AWS Marketplace. Note this is already bypassed after evon --iam-validate is run (which sets the config.ec2_iam_role_status to True), but this prevents the message in dev if the validate has not yet run

--- hub/middleware.py
+++ hub/middleware.py
@@ -3,6 +3,7 @@ from django.utils.safestring import mark_safe
 
 from hub.models import Config
 
+from eapi.settings import EVON_VARS
 
 class HubMiddleware:
 
@@ -11,15 +12,15 @@ class HubMiddleware:
         self.iam_warning_message = 'The EC2 IAM Role for this Hub is not properly configured, functionality will be limited. Please check <a href="/hub/config/1/change/">your Config</a>'
 
     def __call__(self, request):
-        # show warning messsge to superusers if IAM role is improperly configured.
-        if request.user.is_superuser:
+        ### Code that is executed in each request before the view is called
+        # If we're deployed via AWS Marketplace, show warning messsge to superusers if IAM role is improperly configured.
+        if EVON_VARS["hosted_mode"] == "awsmp" and request.user.is_superuser:
             show = True
             storage = messages.get_messages(request)
             for message in storage:
                 if message.message == self.iam_warning_message:
                     show = False
             storage.used = False
-            # Code that is executed in each request before the view is called
             config = Config.get_solo()
             if not config.ec2_iam_role_status and show:
                 messages.add_message(
@@ -35,5 +36,5 @@ class HubMiddleware:
                     "The EC2 IAM Role for this Hub is now configured correctly!",
                 )
         response = self.get_response(request)
-        # Code that is executed in each request after the view is called
+        ### Code that is executed in each request after the view is called
         return response
